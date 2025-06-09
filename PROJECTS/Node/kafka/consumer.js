ab783ff641e657206aca04e6cacfccd28c6e4955
Dmytro Sherstiuk <sherdg@gmail.com>
2025-06-09 16:21:03 +0300

    JS fixes

--- PROJECTS/Node/kafka/consumer.js
+++ PROJECTS/Node/kafka/consumer.js
@@ -1,28 +1,28 @@
-const { Kafka } = require('kafkajs')
+const { Kafka } = require("kafkajs");
 
 const kafka = new Kafka({
-  clientId: 'my-consumer',
-  brokers: ['localhost:9092', 'localhost:9093', 'localhost:9094']
-})
- 
-const consumer = kafka.consumer({ groupId: 'consumer-group' })
-const topic = 'animals'
+  clientId: "my-consumer",
+  brokers: ["localhost:9093", "localhost:9097", "localhost:9098"],
+});
+
+const consumer = kafka.consumer({ groupId: "consumer-group" });
+const topic = "animals";
 
 const run = async () => {
   // Consuming
-  await consumer.connect()
-  
-  await consumer.subscribe({ topic })
- 
+  await consumer.connect();
+
+  await consumer.subscribe({ topic });
+
   await consumer.run({
     eachMessage: async ({ partition, message }) => {
       console.log({
         partition,
         offset: message.offset,
         value: message.value.toString(),
-      })
+      });
     },
-  })
-}
- 
-run().catch(console.error)
+  });
+};
+
+run().catch(console.error);
--- PROJECTS/Node/kafka/producer.js
+++ PROJECTS/Node/kafka/producer.js
@@ -1,36 +1,34 @@
-const { Kafka } = require('kafkajs')
-const Chance = require('chance')
+const { Kafka } = require("kafkajs");
+const Chance = require("chance");
 
-const chance = new Chance()
- 
+const chance = new Chance();
+
 const kafka = new Kafka({
-  clientId: 'my-producer',
-  brokers: ['localhost:9092', 'localhost:9093', 'localhost:9094']
-})
- 
-const producer = kafka.producer()
-const topic = 'animals'
+  clientId: "my-producer",
+  brokers: ["localhost:9093", "localhost:9097", "localhost:9098"],
+});
+
+const producer = kafka.producer();
+const topic = "animals";
 
 const produceMessage = async () => {
-    const value = chance.animal();
-    console.log(value);
-    
-    try {
-      await producer.send({
-          topic,
-          messages: [
-            { value },
-          ],
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+  const value = chance.animal();
+  console.log(value);
+
+  try {
+    await producer.send({
+      topic,
+      messages: [{ value }],
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const run = async () => {
   // Producing
-  await producer.connect()
-  setInterval(produceMessage, 1000)
-}
- 
-run().catch(console.error)
+  await producer.connect();
+  setInterval(produceMessage, 1000);
+};
+
+run().catch(console.error);
